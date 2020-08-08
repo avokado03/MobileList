77751470a3b9050036ba4428130dec7de10bf00c
Ангелина Балашова <angelina.balashova@simbirsoft.com>
2020-08-08 14:42:08 +0300

    Add extentions (dir)

---
+++ WpfApp1/MobileList/Models/Extentions/DirectoriesExtentions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MobileList.Models.Extentions
+{
+    public static class DirectoriesExtentions
+    {
+        public static DirectoriesModel GetAppDirectories(this DirectoriesModel model)
+        {
+            model.ResultDir = AppSettings.ResultPath;
+            model.PDFDir = AppSettings.PDFPath;
+            model.HTMLDir = AppSettings.HTMLPath;
+            return model;
+        }
+
+        public static bool SetAppDirectories(this DirectoriesModel model)
+        {
+            try
+            {
+                Environment.SetEnvironmentVariable("ResultPath", model.ResultDir);
+                Environment.SetEnvironmentVariable("PDFDir", model.PDFDir);
+                Environment.SetEnvironmentVariable("HTMLDir", model.HTMLDir);
+                return true;
+            }
+            catch 
+            {
+                return false;
+            }
+        }
+    }
+}
--- WpfApp1/MobileList/ViewModels/SetDirectoriesVM.cs
+++ WpfApp1/MobileList/ViewModels/SetDirectoriesVM.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace MobileList.ViewModels
 {
@@ -10,9 +11,8 @@ namespace MobileList.ViewModels
     {
         public SetDirectoriesVM()
         {
-
+            
         }
-
 
         public DirectoriesModel Model
         {
@@ -23,10 +23,34 @@ namespace MobileList.ViewModels
             DependencyProperty.Register("Model", typeof(DirectoriesModel), typeof(), new PropertyMetadata(null));
 
         #region CommandProps
+            
         #endregion
 
         #region Command actions
+        //TODO: error handling
+        private void SetDirectory(string field)
+        {
+            try
+            {
+                var dialog = new CommonOpenFileDialog()
+                {
+                    IsFolderPicker = true
+                };
+                var result = dialog.ShowDialog();
+                if (!(result == CommonFileDialogResult.Ok))
+                    throw new ArgumentException();
+                field = dialog.FileName;
+             }
+            catch(ArgumentException ex)
+            {
+                Error = "Беды с папками";
+            }
+            catch (Exception ex)
+            {
+                Error = "Беды с папками";
+            }
+        }
         #endregion
-
+ 
     }
 }
